d9f65ecbec273bedcd1fc806d94a56eb90b36415
Cicero Alvarenga <contatociceroalvarenga@gmail.com>
2024-11-13 21:34:19 +0800

    altera texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>Marcela, te amo muitooooo. Se você me ama, dá uma risadinha!</h1>;
+  return (
+    <h1>Marcela, te amo muitooooo. Se você me ama, dá uma risadinha! 🈴</h1>
+  );
 }
 
 export default Home;
